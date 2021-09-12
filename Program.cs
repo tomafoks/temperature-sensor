e3444a4e1d6b3757b2f5034b0985f94bbb1aba85
Хуснутдинов Тимур Амурович <HusnutdinovTA@int.mechel.corp>
2021-09-12 18:18:15 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace project
+{
+    class project
+    {
+        static void Main()
+        {
+            SerialPort port;
+
+            // получаем список доступных портов
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine("Выберите порт:");
+
+            // выводим список портов
+            for (int i = 0; i < ports.Length; i++)
+            {
+                Console
+                    .WriteLine("[" + i.ToString() + "] " + ports[i].ToString());
+            }
+
+            port = new SerialPort();
+
+            // читаем номер из консоли
+            string n = Console.ReadLine();
+            int num = int.Parse(n);
+
+            try
+            {
+                // настройки порта
+                port.PortName = ports[num];
+                port.BaudRate = 9600;
+                port.DataBits = 8;
+                port.Parity = System.IO.Ports.Parity.None;
+                port.StopBits = System.IO.Ports.StopBits.One;
+                port.ReadTimeout = 500;
+                port.WriteTimeout = 500;
+                port.Open();
+            }
+            catch (Exception e)
+            {
+                Console
+                    .WriteLine("ERROR: невозможно открыть порт:" +
+                    e.ToString());
+                return;
+            }
+
+            port.Write("Hello from C#");
+
+            port.Close();
+        }
+    }
+}
